a7b2a01b40259fc6ab527d1e10cf42479ef0ae4a
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-01-20 20:31:34 +0200

    solve question 6

---
+++ q6.js
@@ -0,0 +1,9 @@
+function dashesNum(num) {
+  let array = num;
+  for (let i = 0; i < array[num].length; i++) {
+    if (num(i % 2 === 0)) {
+      num.push("-");
+    }
+  }
+}
+console.log(0526372);
---
+++ q7.js
@@ -0,0 +1,3 @@
+let sortArr = [""];
+sortArr.sort();
+console.log(-3, 8, 7, 6, 5, -4, 3, 2, 1);
