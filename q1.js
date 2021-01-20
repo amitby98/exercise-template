2c83b400d516c44d97a9b5e0c3bdb109213938aa
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-01-20 17:22:57 +0200

    solve question 1

--- q1.js
+++ q1.js
@@ -1,11 +1,22 @@
-function run() {
-    const input = document.getElementById("input").value;
-    const output = document.getElementById("output");
-    let out = "";
 /**/
-    // write your code here
-    // out = input + 5;
+// write your code here
 
-/**/
-    output.innerText = out;
+function is_array(input) {
+  if (input === [""]) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
+console.log(is_array("w3resource"));
+console.log(is_array([1, 2, 4, 0]));
+
+// let is_array = function (input) {
+//   if (toString.call(input) === "[object Array]") {
+//     return true;
+//     return false;
+//   }
+// };
+// console.log(is_array('w3resource'));
+// console.log(is_array([1, 2, 4, 0]));
