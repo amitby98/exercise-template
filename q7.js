3be2ed34e4c3027146814bca79f1e701dd406539
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-01-21 15:40:27 +0200

    Update q7

--- q7.js
+++ q7.js
@@ -1,3 +1,19 @@
-// let sortArr = [""];
-// sortArr.sort();
-// console.log(-3, 8, 7, 6, 5, -4, 3, 2, 1);
+function sortArr(num) {
+  let notSort = true;
+  while (notSort) {
+    notSort = false;
+    for (let i = 0; i < num.length; i++) {
+      let numPlace = num[i];
+      let placePlus = num[i + 1];
+      if (num[i] > num[i + 1]) {
+        num[i] = placePlus;
+        num[i + 1] = numPlace;
+        notSort = true;
+        console.log(num);
+      }
+    }
+  }
+  return num;
+}
+
+console.log(sortArr([-3, 8, 7, 6, 5, -4, 3, 9, 2, 1]));
