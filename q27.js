9fa62acb0c445feab74d231cd5cf28fa1febd0ee
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-01-21 22:28:12 +0200

    solved q27

--- q27.js
+++ q27.js
@@ -1,1 +1,23 @@
-function propertyValue(array , propertykey) {
+let library = [
+  { author: "Bill Gates", title: "The Road Ahead", libraryID: 1254 },
+  { author: "Steve Jobs", title: "Walter Isaacson", libraryID: 4264 },
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    libraryID: 3245,
+  },
+];
+
+function allProperty(object, prop) {
+  let result = [];
+  for (let obj of object) {
+    if (obj.hasOwnProperty(prop)) {
+      result.push(obj[prop]);
+    } else {
+      result.push("not exist");
+    }
+  }
+  console.log(result);
+}
+console.log("src -> (library, 'title')");
+allProperty(library, "title");
